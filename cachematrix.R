ea2cd6b890bd40bbbf112800827ccb42e2f445f8
Ethosien <swartzel.ja@gmail.com>
2015-10-22 11:00:11 -0400

    This is my 3rd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Coursera RProgramming Course (rprog-033) assignment 2 by Jeff Swartzel, 10-22-15
+# This script contains 2 functions for programming assignment 2.
+# Together these functions can be used to cache the potentially 
+# time consuming calculation of inverting a matrix.
 
-## Write a short comment describing this function
+#############
 
-myMatrix<-matrix(data=c(2,3,2,1,2,1,1,1,2),ncol=3,nrow=3)
+# This function, makeCacheMatrix(), will create a list containing 4 separate functions
+# that are used to create special objects that will store a matrix and cache its inverse.
+# 1. set() will set the value of the matrix
+# 2. get() will get the value of the matrix
+# 3. getInverse() will get the value of the inverse matrix
+# 4. setInverse() will set the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -15,19 +22,39 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setInverse <- function(inverse) m <<- inverse
   getInverse <- function() m
-  list(set=set,
-       get=get,
+  list(set=set, get=get,
        setInverse=setInverse,
        getInverse=getInverse)
 
 }
 
+#############
 
-## Write a short comment describing this function
+# This function, cacheSolve(), calculates the inverse of the 
+# special matrix created by makeCacheMatrix.  However, it will first check to see 
+# if the inverse has already been calculated.  If the inverse has already been 
+# calculated, it will retrieve the already calculated inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting cached inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setInverse(m)
+  # Return a matrix that is the inverse of 'x'
+  m
 }
 
+#############
 
-#this is my 2nd commit
+# This code will create a large, invertible matrix called "myMatrix".
+# myMatrix<-matrix(runif(3000^2),3000)
+# This can be used to test the functions. 
+
+#############
+
+#this is my 3rd commit
