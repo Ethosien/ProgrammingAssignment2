898c739049f45e90ecf7e6c0a7976f87f9d14a07
Ethosien <swartzel.ja@gmail.com>
2015-10-22 08:52:38 -0400

    2nd commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,22 @@
 
 ## Write a short comment describing this function
 
+myMatrix<-matrix(data=c(2,3,2,1,2,1,1,1,2),ncol=3,nrow=3)
+
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set<-function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set=set,
+       get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
 
 }
 
@@ -15,4 +30,4 @@ cacheSolve <- function(x, ...) {
 }
 
 
-#this is my 1st commit
+#this is my 2nd commit
